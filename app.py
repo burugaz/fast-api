ef5fa97a6453edbb1163a77c1227a59113f4890b
Andrew <andygim625@gmail.com>
2026-03-01 20:46:13 +0300

    Task 1.4

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from models import User
 
 app = FastAPI()
 
@@ -7,7 +8,13 @@ class Numbers(BaseModel):
     num1: float
     num2: float
 
-@app.get("/calculate")
+user = User(name="Дементьев Андрей", id=1)
+
+@app.post("/calculate")
 async def calculate(numbers: Numbers):
     result = numbers.num1 + numbers.num2
     return {"result": result}
+
+@app.get("/users")
+async def get_user():
+    return user
