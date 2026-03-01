ad811a3d49f6ce44e5b498feee87144f6f88e16d
Andrew <andygim625@gmail.com>
2026-03-01 20:58:41 +0300

    Task 1.5*

--- app.py
+++ app.py
@@ -1,20 +1,48 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from models import User
+from models import User, UserWithAge
 
 app = FastAPI()
 
+# №1.1
+@app.get("/")
+async def root():
+    return {"message": "Добро пожаловать в моё приложение FastAPI!"}
+
+
+# №1.2
+@app.get("/")
+async def get_index():
+    return FileResponse("index.html")
+
+
+# №1.3
 class Numbers(BaseModel):
     num1: float
     num2: float
-
-user = User(name="Дементьев Андрей", id=1)
 
 @app.post("/calculate")
 async def calculate(numbers: Numbers):
     result = numbers.num1 + numbers.num2
     return {"result": result}
 
+
+# №1.4
+user = User(name="Дементьев Андрей", id=1)
+
 @app.get("/users")
 async def get_user():
     return user
+
+
+# №1.5
+@app.post("/user")
+async def check_adult(user: UserWithAge):
+    is_adult = user.age >= 18
+
+    return {
+        "name": user.name,
+        "age": user.age,
+        "is_adult": is_adult
+    }
